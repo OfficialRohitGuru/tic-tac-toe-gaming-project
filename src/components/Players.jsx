f88a657c91c24ebd33dc207534d8fc81f98e4e39
Rohit Guru <officialrohitguru@gmail.com>
2024-06-01 07:14:35 +0530

    Final submission

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({initialName , symbol}) {
+export default function Player({initialName , symbol, isActive, onChangeName}) {
   const [playerName, setPlayerName] = useState(initialName); 
   
   const [isEditing , setIsEditing] = useState(false);
@@ -9,6 +9,8 @@ export default function Player({initialName , symbol}) {
         // setIsEditing(isEditing ? false : true);
         // setIsEditing(!isEditing)
         setIsEditing((editing)=>!editing)
+        if(isEditing){ onChangeName(symbol, playerName);}
+       
     }
 
     function handleChange(event){
@@ -25,7 +27,7 @@ export default function Player({initialName , symbol}) {
     }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
